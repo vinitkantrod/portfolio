ed80eb705b9d4d06054a1b3314e0d6b8e0f58adc
vinit kantrod <kantrod.vinit@gmail.com>
2024-05-23 15:32:49 +0530

    started about section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import Header from '@/components/header';
+import Header from '../components/header';
+import Home from '../components/home';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,10 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-100 text-gray-950 relative h-[5000px]`}>
+      <body className={`${inter.className} bg-gray-100 text-gray-950 relative h-[5000px] pt-28 sm:pt-26`}>
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
         <Header />
+        <Home />
         {children}
       </body>
     </html>
---
+++ components/home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Intro from './intro'
+import Header from './header'
+
+export default function Home() {
+    return (
+        <Intro />
+    )
+}

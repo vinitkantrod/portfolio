7472264f1df3596c1d5efd52c90766d29c95119c
vinit kantrod <kantrod.vinit@gmail.com>
2024-05-23 16:38:56 +0530

    completed section divider

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Intro from "../components/intro";
-import Header from "../components/header";
+import SectionDivider from "@/components/section-divider";
 
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
+      <SectionDivider />
     </main>
   )
 }
